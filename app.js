d4d9a047ef4103bfeb771613e0cb43c4084a1173
Mabel Rojas <mabel.rojasmejia@gmail.com>
2025-08-11 10:20:54 -0600

    WIP antes de crear develop

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Crear un arry para almacenar los nombres
+    let amigos =[];
+// función para la lista de amigos
+function agregarAmigo(){
+    // Capturar el valor del campo de entrada
+    let  input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+    console.log(nombre);
+    // Validar la entrada
+    if (nombre == ""){
+        alert('Por favor, inserte un nombre.');
+    }else{
+        //Actualizar el array de amigos
+          amigos.push(nombre);
+          console.log(amigos);
+        // Limpiar el campo de entrada
+          limpiarCaja();
+           mostrarLista(); 
+    }
+} 
